e917b8c773f18c6a75a7598638320f54d4054c57
Fran Viñas <fvinas@fi.uba.ar>
2021-12-14 18:34:36 -0300

    Server socket is in RoomManager

--- server/Server.h
+++ server/Server.h
@@ -5,12 +5,12 @@
 
 class Server {
 private:
-    Socket acceptor;
+    std::string service;
 public:
     /*
      *  Constructor
      */
-    explicit Server(const char *service);
+    explicit Server(const std::string service);
     /*
      *  Lanza el hilo con el RoomManager y se queda leyendo de entrada estandar.
      *  Una vez que lee el caracter 'q', detiene la ejecucion del hilo
